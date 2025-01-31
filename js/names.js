e5cedb0e41849dd888f9b3a5f570015ce8f78012
Jonathanmasa <jonathanmasa97@gmail.com>
2025-01-31 16:29:15 +0100

    feat[names.js]

--- js/names.js
+++ js/names.js
@@ -1,2 +1,7 @@
 // 1 - names.js
 // 1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName
+
+function createPerson(firstName, lastName) {
+    return { firstName, lastName };
+}
+module.exports = createPerson;
