4ec904637612026323e50d4330d9bbbbadfea458
Jonathanmasa <jonathanmasa97@gmail.com>
2025-01-31 17:10:00 +0100

    feat[fullname-hobbies]

--- js/people.js
+++ js/people.js
@@ -13,3 +13,15 @@ const hobbys = require('./hobbies');
 // stampo risultato a terminale
 console.log(hobbys("hobbyOne", "hobbyTwo", "hobbyThree"));
 
+
+// 3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All’interno della tua funzione, usa le tue due funzioni precedenti per costruire l’oggetto.
+
+function namesHobby() {
+    const fullName = createPerson("Jonathan", "Masa");
+    const hobbies = hobbys("hobbyOne", "hobbyTwo", "hobbyThree");
+    return  { fullName: fullName, hobbies: hobbies };
+}
+// stampo risultato a terminale
+console.log(namesHobby());
+
+
